1383b0c8bc5a40ceb2eebe94d8f9afd52a58cdff
Felipe Lima <felipeolimadev@gmail.com>
2025-12-19 08:56:26 -0300

    Adds IDE prompt rule and updates AndroidX Lifecycle to 2.10.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,8 +57,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.material3)
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
